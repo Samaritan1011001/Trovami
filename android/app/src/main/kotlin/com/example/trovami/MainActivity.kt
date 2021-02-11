bd467c3d97e78e1a12f58bf1660b0a9ff15201ff
Manoj <manojnb95@gmail.com>
2021-02-10 23:01:57 -0600

    fix: migrates the app to android embedding v2

--- android/app/src/main/kotlin/com/example/trovami/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/trovami/MainActivity.kt
@@ -2,12 +2,7 @@ package com.example.trovami
 
 import android.os.Bundle
 
-import io.flutter.app.FlutterActivity
+import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
-}
+class MainActivity: FlutterActivity() {}
